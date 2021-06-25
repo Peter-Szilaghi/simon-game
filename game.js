9281f1c1ef202eb9200cfd9b9d45ac01fb3f7678
Peter Szilaghi <85834754+Peter-Szilaghi@users.noreply.github.com>
2021-06-25 20:54:24 +0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,66 @@
+let colors = ["green", "red", "yellow", "blue"]; //buttons color for class, random number and audio 
+let game = []; //save Random Numbers; 
+let clickCount = 0;
+let startGame = false;
+
+function randomNum() {
+
+    let num = Math.round( Math.random() * 3 ); //Random Number
+    let colorNum = colors[num]; //Colors for Random Number
+
+    playSound(colorNum);
+    buttonAnimation(`#${ colorNum }`, "pressed");
+    game.push( colorNum ); //Add Random Number to array
+
+    $("h1").text( `Level ${ game.length }` );
+}
+
+$(document).keypress( ()=> { //PlayAgain
+    if (!startGame) {
+        randomNum();
+        startGame = true;
+    }
+});
+
+$("[type = button]").click( function() { 
+    let curentClick = $(this).attr("id");
+    buttonAnimation(this, "pressed");
+
+    if ( $(this).is( `#${ game[ clickCount ] }` ) ) { //Verify colors
+
+        playSound(curentClick);
+        if ( clickCount === game.length - 1 ) { //If all true go to next Lvl
+            setTimeout(() => {
+                randomNum();
+            }, 500);
+            clickCount = 0;
+        } else { //If true click the next color
+            clickCount++;
+        }
+                
+    } else { //IF not match the colors, Restart pressing any key
+        $("h1").text("Game Over, Press Any Key to Restart");
+        playSound("wrong");
+        buttonAnimation("body", "red");
+        ResetGame();
+    }
+
+} );
+
+
+function playSound(sound){
+    new Audio( `sounds/${sound}.mp3` ).play();
+}
+
+function buttonAnimation(e, anim){
+    $(e).addClass(anim); 
+    setTimeout(() => { 
+        $(e).removeClass(anim);
+    }, 100);
+}
+
+function ResetGame(){
+    game = [];
+    clickCount = 0;
+    startGame = false;
+}
